3be6ffd150e0737174895a6fab10f8c139b273cd
akshayb407 <akshayb407@gmail.com>
2022-07-26 13:25:40 +0530

    UC3_Add_PartTimeEmp_EmpWage

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -7,17 +7,25 @@ namespace EmpWage
         static void Main(string[] args)
         {
             int isFullTime = 1;
+            int isPartTime = 2;
             int perHrWage = 20;
-            int empHrs = 8;
+            int empHrs = 0;            
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if(empCheck == isFullTime)
             {
+                Console.WriteLine("Employee is Present isFullTime");
                 empHrs = 8;
+            }
+            else if(empCheck == isPartTime)
+            {
+                Console.WriteLine("Employee is Present isPartTime");
+                empHrs = 4;
             }
             else
             {
+                Console.WriteLine("Employee is Absent");
                 empHrs = 0;
             }
             empWage = empHrs * perHrWage;
