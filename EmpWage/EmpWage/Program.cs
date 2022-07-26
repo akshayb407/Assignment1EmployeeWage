73b4186ca0624ac4103198af17a5de789b96750b
akshayb407 <akshayb407@gmail.com>
2022-07-26 12:32:22 +0530

    UC1_Check_Emp_Presence added

---
+++ EmpWage/EmpWage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.WriteLine("Check Value of Employee is Present or Absent =" +empCheck);
+        }
+
+    }
+}
