4e3a63f751abbbf7fd19bd8bee94c0585eaefcf0
akshayb407 <akshayb407@gmail.com>
2022-08-02 00:47:11 +0530

    UC7_Refactor_Code_ClassMethod_Class_variables

---
+++ EmpWage/EmpWage/EmployeeWages.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class EmployeeWages
+    {
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int perHrWage = 20;
+        public const int numOfWorkingDays = 20;
+        public const int maxHrsInMonth = 100;
+        public void EmployeeWage()
+        {
+
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * perHrWage;
+            Console.WriteLine("Total Employee Wage in Month =" + totalEmpWage);
+        }
+
+    }
+}
--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -4,40 +4,13 @@ namespace EmpWage
 {
     internal class Program
     {
-        public const int isFullTime = 1;
-        public const int isPartTime = 2;
-        public const int perHrWage = 20;
-        public const int numOfWorkingDays = 20;
-        public const int maxHrsInMonth = 100;
+
         static void Main(string[] args)
         {
-          
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case isPartTime:
-                        empHrs = 4;
-                      break;
-
-                    case isFullTime:
-                        empHrs = 8;
-                       break;
-
-                    default:
-                        empHrs = 0;               
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays +"Emp Hrs : "  + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * perHrWage;
-            Console.WriteLine("Total Employee Wage in Month =" + totalEmpWage);
+            EmployeeWages employees = new EmployeeWages();
+            employees.EmployeeWage();
         }
+
 
     }
 }
